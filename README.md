d7736a20d37455fda5f468830841f5d2ff269112
RajeswariManohar <71829809+RajeswariManohar@users.noreply.github.com>
2020-12-28 17:27:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Arnavc20
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedRect,movingRect;
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(200, 200, 50, 80);
+  movingRect=createSprite(400, 200, 80, 30);
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+  fixedRect.debug=true;
+  movingRect.debug=true;
+}
+
+function draw() {
+  background(0);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    &&fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2)
+  {
+    fixedRect.shapeColor="red";
+  movingRect.shapeColor="red";
+  }
+  else{
+    fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+  }
+  drawSprites();
+}
